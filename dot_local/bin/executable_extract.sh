edeec570d46eebc3d46bed72e164638bfc0d2483
Arran Ubels <arran4@gmail.com>
2025-06-04 20:59:44 +1000

    Your user’s .npmrc file (${HOME}/.npmrc) has a `globalconfig` and/or a `prefix` setting, which are incompatible with nvm. Run `nvm use --delete-prefix v22.16.0 --silent` to unset it. Merge main

--- dot_local/bin/executable_extract.sh
+++ dot_local/bin/executable_extract.sh
@@ -3,23 +3,23 @@
 set -e
 
 for f in "$@"; do
-    if [ -f "$f" ]; then
-        case "$f" in
-        *.tar.bz2)   tar -xvjf  "$f"    ;;
-        *.tar.gz)    tar -xvzf  "$f"    ;;
-        *.tar.xz)    tar -xvJf  "$f"    ;;
-        *.bz2)       bunzip2    "$f"    ;;
-        *.rar)       rar x      "$f"    ;;
-        *.gz)        gunzip     "$f"    ;;
-        *.tar)       tar -xvf   "$f"    ;;
-        *.tbz2)      tar -xvjf  "$f"    ;;
-        *.tgz)       tar -xvzf  "$f"    ;;
-        *.zip)       unzip      "$f"    ;;
-        *.Z)         uncompress "$f"    ;;
-        *.7z)        7z x       "$f"    ;;
-        *)           echo "don't know how to extract '$f'..." ;;
-        esac
-    else
-        echo "'$f' is not a valid file!"
-    fi
+  if [ -f "$f" ]; then
+    case "$f" in
+    *.tar.bz2)   tar -xvjf  "$f"    ;;
+    *.tar.gz)    tar -xvzf  "$f"    ;;
+    *.tar.xz)    tar -xvJf  "$f"    ;;
+    *.bz2)       bunzip2    "$f"    ;;
+    *.rar)       rar x      "$f"    ;;
+    *.gz)        gunzip     "$f"    ;;
+    *.tar)       tar -xvf   "$f"    ;;
+    *.tbz2)      tar -xvjf  "$f"    ;;
+    *.tgz)       tar -xvzf  "$f"    ;;
+    *.zip)       unzip      "$f"    ;;
+    *.Z)         uncompress "$f"    ;;
+    *.7z)        7z x       "$f"    ;;
+    *)           echo "don't know how to extract '$f'..." ;;
+    esac
+  else
+    echo "'$f' is not a valid file!"
+  fi
 done
