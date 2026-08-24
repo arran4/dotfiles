7d115ba59b7e80fd37dda98eed7173d8d20419f7
Arran Ubels <arran4@gmail.com>
2026-08-24 21:48:31 +1000

    hyprland: set x1linux monitor scale to 1.67

--- dot_config/hypr/local_x1linux.lua
+++ dot_config/hypr/local_x1linux.lua
@@ -3,11 +3,11 @@ hl.monitor({
   output = "eDP-1",
   mode = "preferred",
   position = "0x0",
-  scale = 1.5,
+  scale = 1.67,
 })
 hl.monitor({
   output = "",
   mode = "preferred",
   position = "auto",
-  scale = 1.5,
+  scale = 1.67,
 })
